fc52947fe042a9336a5ff7b417cde33ad7d0d75f
iivanovw7 <9iiiuk2@gmail.com>
2023-02-19 23:20:45 +0000

    feat: Adds basic feature sliced config for testing.

---
+++ feature-sliced.js
@@ -0,0 +1,18 @@
+module.exports = {
+    'extends': [
+        require.resolve('./rules/feature-sliced')
+    ],
+    'settings': {
+        'boundaries/elements': [
+            { 'type': 'app', 'pattern': 'app/*' },
+            { 'type': 'processes', 'pattern': 'processes/*' },
+            { 'type': 'pages', 'pattern': 'pages/*' },
+            { 'type': 'widgets', 'pattern': 'widgets/*' },
+            { 'type': 'features', 'pattern': 'features/*' },
+            { 'type': 'entities', 'pattern': 'entities/*' },
+            { 'type': 'shared', 'pattern': 'shared/*' }
+        ],
+        'boundaries/ignore': ['**/*.test.*'],
+    },
+    'overrides': [ { 'files': ['**/*.test.*'], 'rules': { 'boundaries/element-types': 'off' } } ]
+};
