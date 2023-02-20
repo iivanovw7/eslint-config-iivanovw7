6b0791d3d10d2da32ba3ff1d4272980040cefbd8
iivanovw7 <9iiiuk2@gmail.com>
2023-02-20 20:02:47 +0000

    feat: Removes airbnb-typescript.

--- typescript.js
+++ typescript.js
@@ -1,6 +1,5 @@
 module.exports = {
     'extends': [
-        'airbnb-typescript',
         'plugin:@typescript-eslint/recommended',
         'plugin:@typescript-eslint/recommended-requiring-type-checking',
         'plugin:import/typescript',
