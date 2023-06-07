e22ca95346e608b2c07507c1549abd1840cd0b95
Igor <igor.i_ivanov@mytona.com>
2023-06-07 22:46:54 +0100

    fix: :bug: rule fix

--- rules/typedoc.js
+++ rules/typedoc.js
@@ -1,6 +1,8 @@
 module.exports = {
-    'require-jsdoc': 2,
-    'jsdoc/require-description': 2,
-    'jsdoc/require-returns': 2,
-    'jsdoc/require-returns-description': 2,
+    rules: {
+        'require-jsdoc': 2,
+        'jsdoc/require-description': 2,
+        'jsdoc/require-returns': 2,
+        'jsdoc/require-returns-description': 2,
+    }
 };
